d17513eea79de619bb138c67d62f80e3c53a7aa1
gurshaan17 <gurshaansinghd@gmail.com>
2024-06-18 22:43:25 +0530

    routing done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,26 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/signup',(c)=>{
+  return c.text('sign up')
+})
+
+app.post('/api/v1/signin',(c)=>{
+  return c.text('sign in')
+})
+
+app.get('/api/v1/blog/:id',(c)=>{
+  const id = c.req.param
+  console.log(id);
+  return c.text('blog')
+})
+
+app.post('/api/v1/blog',(c)=>{
+  return c.text('read')
+})
+
+app.put('/api/v1/blog',(c)=>{
+  return c.text('read')
 })
 
 export default app
