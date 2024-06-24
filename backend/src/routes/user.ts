98b95b541226be710d8c58f1a572fc9d2baa4f68
gurshaan17 <gurshaansinghd@gmail.com>
2024-06-25 01:07:13 +0530

    common deleted

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
 import { Hono } from 'hono';
 import { sign } from 'hono/jwt';
-
+import { signinInput , signupInput } from '@100xdevs/medium-common';
 
 export const userRouter = new Hono<{
     Bindings:{
@@ -16,8 +16,12 @@ userRouter.post('/signup', async (c) => {
     const prisma = new PrismaClient({
           datasourceUrl: c.env.DATABASE_URL,
       }).$extends(withAccelerate());
-  
     const body = await c.req.json();
+    const { success } = signupInput.safeParse(body)
+    if(!success){
+      c.status(411)
+      return c.text("incorrect inputs sent")
+    }
     try {
       const user = await prisma.user.create({
         data: {
@@ -42,6 +46,11 @@ userRouter.post('/signup', async (c) => {
       }).$extends(withAccelerate());
   
     const body = await  c.req.json()
+    const { success } = signinInput.safeParse(body)
+    if(!success){
+      c.status(411)
+      return c.text("incorrect inputs sent")
+    }
     try{
       const userExists = await prisma.user.findUnique({
         where:{
